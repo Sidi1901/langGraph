930923ff441837003a4673bf7bb20c8896ae8c68
Sidi1901 <siddhi190100@gmil.com>
2026-06-02 03:30:41 +0530

    Added Chains intro

---
+++ Lesson04-Chains/simple-chain.py
@@ -0,0 +1,26 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=2.0    # temperature must be in the range [0.0, 2.0]
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({"topic":"GTA V"})
+
+print(result)
